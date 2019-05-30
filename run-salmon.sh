72606f0ed00d8d2e62b7067028ac0dbcfad08e29
Austin Hammond <austin.hammond@usask.ca>
2019-05-30 16:34:27 -0600

    drop salmon index path

--- run-salmon.sh
+++ run-salmon.sh
@@ -4,7 +4,6 @@ r1=$1;shift
 r2=$1; shift
 prefix=$1; shift
 index=$1
-#index=/mnt/NGSF001/analysis/indices/human/gencode-30/salmon/gencode.v30.transcripts.index
 
 /home/ahammond/src/salmon-latest_linux_x86_64/bin/salmon quant -i $index -l A -1 $r1 -2 $r2 \
 	--validateMappings \
