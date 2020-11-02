278315fa5e937175732663509ee57f9faf944dfb
Austin Hammond <austin.hammond@usask.ca>
2020-11-02 08:50:48 -0600

    update salmon version

--- run-salmon.sh
+++ run-salmon.sh
@@ -5,7 +5,7 @@ r2=$1; shift
 prefix=$1; shift
 index=$1
 
-/home/ahammond/src/salmon-latest_linux_x86_64/bin/salmon quant -i $index -l A -1 $r1 -2 $r2 \
+/home/ahammond/src/salmon-1.3.0/bin/salmon quant -i $index -l A -1 $r1 -2 $r2 \
 	--validateMappings \
 	-o ${prefix}_salmon_quant \
 	-p 6 \
