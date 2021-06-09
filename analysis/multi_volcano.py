d156e0c1011f588ec66a4e4dca3fcbac084ff7bb
Your Name <acboulet@outlook.com>
2021-06-09 15:46:11 -0600

    multi_volcano modifiied to accept command line -> tested with 3-set data

--- analysis/multi_volcano.py
+++ analysis/multi_volcano.py
@@ -1,34 +1,58 @@
 # %%
-# Interactive python script created in vscode
-# Uses plotly to build volcano plot overlaying multiple datasets
-
-# TODO add ability to select specific gene
-
-# %%
 import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
 import numpy as np
+import argparse
+
+# LIMIT will never be able to select specific gene because limited by Scatter function. To employ Dash requires using a host server
+# such as heroku
+
 
 # %%
-# Add name of data traces, file names, and final graph name
-names = ["d4_vs_d0", "d1_vs_d0", "d4_vs_d1"]
-files = ["experiment_d4_vs_d0_deseq2_DE_only.csv", "experiment_d1_vs_d0_deseq2_DE_only.csv",
-            "experiment_d4_vs_d1_deseq2_DE_only.csv"]
-graph_name = "all_days"
+# Use command line functions
+def get_args():
+    """
+    Purpose:
+        Pull arguments from command line
+    Return:
+        arguments:
+            .name : name of final graph
+            .files : list of dataset name and file separated by space
+                ex: dataset1 dataset1.csv
+    """
+    parser = argparse.ArgumentParser(description='Provide input for volcano plot')
+    parser.add_argument('name', help='name of graph')
+    parser.add_argument('files', help='file-of-file input, one on each line')
+
+    arguments = parser.parse_args()
+    return arguments
+
+args = get_args()
+graph_name = args.name
+
 
 # Grab all the data and convert into dataframe
 dfs = []
-for file in files:
-    df = pd.read_csv(file, header=0)
-    
-    # Create x-axis values for log2 and then multiply by -1 if "DOWN" regulated
-    df["log2(fold_change)"] = np.log2(df["fold_change"])
-    df["log2(fold_change)"] = np.where(df["change_direction"] == "DOWN", df["log2(fold_change)"] * -1, df["log2(fold_change)"])
+names = []
 
-    # Create y-axis values for -log10 of the p value
-    df["-log10(padj)"] = np.log10(df["padj"])*-1
-    dfs.append(df)
+fof = args.files
+
+with open(fof, 'r') as files:
+    for file in files:
+        # Take each line. Split, first item becomes the datasets name and the second one is stripped/cast and opened for a dataframe
+        file = file.split(" ")
+        names.append(file[0])
+        file = str(file[1].strip())
+        df = pd.read_csv(file, header=0)
+        
+        # Create x-axis values for log2 and then multiply by -1 if "DOWN" regulated
+        df["log2(fold_change)"] = np.log2(df["fold_change"])
+        df["log2(fold_change)"] = np.where(df["change_direction"] == "DOWN", df["log2(fold_change)"] * -1, df["log2(fold_change)"])
+
+        # Create y-axis values for -log10 of the p value
+        df["-log10(padj)"] = np.log10(df["padj"])*-1
+        dfs.append(df)
 
 # %%
 # Create a list of every gene name in the DE files
@@ -41,8 +65,6 @@ for df in dfs:
 gene_names = list( dict.fromkeys(gene_names) )
 gene_names.sort()
 
-
-# %%
 
 # %%
 # create the graphs
@@ -85,7 +107,7 @@ layout = go.Layout(
 
 fig = go.Figure(data=data, layout=layout)
 
-
-pyo.plot(fig, filename=graph_name + "_volcano.html",
-        )
 # %%
+if __name__ == "__main__":
+    pyo.plot(fig, filename=graph_name + "_arg_volcano.html",
+            )
