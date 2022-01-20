ad3f8efdd887bc200a2089dac22079d4429c7b81
Aren Boulet <acboulet@outlook.com>
2022-01-20 15:59:39 -0600

    deseq2_htseq.py is being modified >goal is accept json from node > will need to convert to a class where project_info can be an attribute

--- pipe_control/deseq2_htseq.py
+++ pipe_control/deseq2_htseq.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import numpy as np
 import os
+from json import load
+
 
 from py_deseq import py_DESeq2
+    
 
+
+# TURN THIS INTO A CLASS 
+
+project_info = []
+
+def read_json(filename: str):
+    
+    # how to get this to update variable outside of scope of function
+    with open(filename, "r") as read_file:
+        project_info.append(load(read_file))
 
 def create_count_matrix(libraries: list, outdir: str) -> pd.DataFrame:
     """
@@ -29,11 +42,29 @@ def create_count_matrix(libraries: list, outdir: str) -> pd.DataFrame:
     return df
 
 def create_design_matrix(list_matrix: list) -> pd.DataFrame:
+    """
+    Purpose:
+        Create a design matrix from a provided python list
+    Pre-cond:
+        :param list_matrix: deseq2 design matrix in python list format
+    Return:
+        Design matrix in pd.DataFrame which contains design matrix in correct format for rpy2
+    """
     df = pd.DataFrame(data=list_matrix[1:], columns=list_matrix[0])
     df = df.set_index("sample")
     return df
 
 def deseq2(count_matrix, design_matrix):
+    """
+    Purpose
+        Runs deseq2 using provided count and design matrix.
+    TODO: adjust for contrast as well? what other deseq parameters are important? can add to json
+    Pre:
+        :param count_matrix: pd.DataFrame from .create_count_matrix()
+        :param design_matrix: pd.DataFrame from .create_design_matrix()
+    Return:
+        TODO: will print to a csv file
+    """
     dds = py_DESeq2(count_matrix=count_matrix[count_matrix["R2100207"] != 0], # Remove any that contain 0s
                     design_matrix=design_matrix,
                     design_formula="~ treatment",
@@ -45,12 +76,11 @@ def deseq2(count_matrix, design_matrix):
     print(res.head())
 
 def main():
-    count_matrix = create_count_matrix(["R2100207", "R2100208", "R2100209", "R2100210"], ".")
-    design_matrix = create_design_matrix([["sample", "treatment", "other"],
-                    ["R2100207", "control", "other1"],
-                    ["R2100208", "control", "other1"],
-                    ["R2100209", "treated", "other2"],
-                    ["R2100210", "treated", "other2"]])
+    read_json("./example_support.json")
+    project_info = project_info[0]
+    print(project_info)
+    count_matrix = create_count_matrix(project_info["samples"], ".")
+    design_matrix = create_design_matrix(project_info["conditions"])
     deseq2(count_matrix, design_matrix)
     # print(len(count_matrix.index))
     # # df = count_matrix.loc[((count_matrix!=0).any(axis=1))]
