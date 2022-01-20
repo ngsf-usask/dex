c03f1a8b2ce911a84a667b1fc92dd61c83651c29
Aren Boulet <acboulet@outlook.com>
2022-01-20 14:40:43 -0600

    added commenting and clean up

--- pipe_control/batch_pipe_deseq2.sh
+++ pipe_control/batch_pipe_deseq2.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
-# Installs deseq2 on the node, and runs using python script
-# https://github.com/wckdouglas/diffexpr
+# Runs on deseq2 using htseq output on plato, but using the rpy2 module
 
-#SBATCH --job-name="NGSF_RNA"
+#SBATCH --job-name="htseq2"
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=0:20:00
-#SBATCH --mem=4G
-
-set -eux
+#SBATCH --cpus-per-task=2
+#SBATCH --time=00:20:00
+#SBATCH --mem=20G
 
 # conda activate diffexpr
 
-
-# Transfer files, and verify installation
-mkdir ${SLURM_TMPDIR}/diffexpr
-rsync -rvzP /datastore/NGSF001/software/src/diffexpr/diffexpr-master/* ${SLURM_TMPDIR}/diffexpr/
-
-
-# Use direct paths to env created in conda 
-# /globalhome/arb594/HPC/anaconda3/envs/diffexpr/bin/Rscript ${SLURM_TMPDIR}/diffexpr/setup.R
-# /globalhome/arb594/HPC/anaconda3/envs/diffexpr/bin/python ${SLURM_TMPDIR}/diffexpr/setup.py install
-
-
-/globalhome/arb594/HPC/anaconda3/envs/diffexpr/bin/python /globalhome/arb594/HPC/cluster/pipe_RNA/pipe_control/deseq2_htseq.py
-# TODO adjust path in future
+python /globalhome/arb594/HPC/cluster/pipe_RNA/pipe_control/deseq2_htseq.py
--- pipe_control/deseq2_htseq.py
+++ pipe_control/deseq2_htseq.py
@@ -2,10 +2,19 @@ import pandas as pd
 import numpy as np
 import os
 
-# from py_deseq import py_DESeq2
+from py_deseq import py_DESeq2
 
 
-def combine_results(libraries: list, outdir: str) -> pd.DataFrame:
+def create_count_matrix(libraries: list, outdir: str) -> pd.DataFrame:
+    """
+    Purpose:
+        Combines htseq results from all provided libraries in a given directory. 
+    Pre:
+        :param libraries: List of all libraries to analyze
+        :param outdir: Relative pathway to project pathway.
+    Return:
+        Pandas dataframe containing the gene names and the htseq counts for each library.
+    """
     # Create the first dataframe
     # TODO: SHOULD I REMOVE ANY 0s?
     df = pd.read_table(f"{outdir}/alignment/{libraries[0]}/{libraries[0]}.counts.htseq.txt", 
@@ -24,11 +33,30 @@ def create_design_matrix(list_matrix: list) -> pd.DataFrame:
     df = df.set_index("sample")
     return df
 
+def deseq2(count_matrix, design_matrix):
+    dds = py_DESeq2(count_matrix=count_matrix[count_matrix["R2100207"] != 0], # Remove any that contain 0s
+                    design_matrix=design_matrix,
+                    design_formula="~ treatment",
+                    gene_column="Gene")
+
+    dds.run_deseq()
+    dds.get_deseq_result(contrast=["treatment", "control", "treated"])
+    res = dds.deseq_result
+    print(res.head())
+
 def main():
-    # results = combine_results(["R2100207", "R2100208"], ".")
+    count_matrix = create_count_matrix(["R2100207", "R2100208", "R2100209", "R2100210"], ".")
     design_matrix = create_design_matrix([["sample", "treatment", "other"],
                     ["R2100207", "control", "other1"],
-                    ["R2100208", "treated", "other2"]])
+                    ["R2100208", "control", "other1"],
+                    ["R2100209", "treated", "other2"],
+                    ["R2100210", "treated", "other2"]])
+    deseq2(count_matrix, design_matrix)
+    # print(len(count_matrix.index))
+    # # df = count_matrix.loc[((count_matrix!=0).any(axis=1))]
+    # # df = count_matrix[count_matrix["R2100207"] != 0]
+    # print(len(df.index))
+
 
 if __name__ == "__main__":
     print("Testing?")
